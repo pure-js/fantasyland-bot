f589bf215f05a182c4f68555cf816eb2c279a8b4
pure-js <evolution.is.already.underway@gmail.com>
2016-12-08 06:01:36 +0300

    refactoring

--- src/fighter.js
+++ src/fighter.js
@@ -1,36 +1,48 @@
-function chooseRandom(min = 0, max = 2) {
-  return Math.floor(Math.random() * (max + 1 - min));
+const config = {
+  minDelay: 0,
+  maxDelay: 3700,
+  leave: true
 }
 
-(function fight(maxDelay) {
-  let frameset = document.querySelector('frameset');
-  let loc = frameset.querySelector('frame#loc').contentWindow.document;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-  let army = loc.querySelector('#your_army').contentWindow.document;
-  let combat = loc.querySelector('#combat_panel').contentWindow.document;
-
+function getFollower() {
+  let location = document.querySelector('#loc').contentWindow.document;
+  let army = location.querySelector('#your_army').contentWindow.document;
   let solders = army.querySelectorAll('td.cp');
-  let log = combat.querySelector('#log');
 
-  let i = chooseRandom();
-  solders[i].click();
+  let i = getRandomInt(0, 2);
+
+  return solders[i];
+}
+
+(function fight(config) {
+  let location = document.querySelector('#loc').contentWindow.document;
+  let combat = location.querySelector('#combat_panel').contentWindow.document;
+  let combatLog = combat.querySelector('#log');
+
+  getFollower().click();
 
   let observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
       setTimeout(function() {
-        let j = chooseRandom();
-        solders[j].click();
-      }, chooseRandom(0, 4000))
+        getFollower().click();
+      }, getRandomInt(config.minDelay, config.maxDelay));
+
+      let leaveEl = combat.querySelector('#la a');
+
+      if (leaveEl && config.leave) {
+        observer.disconnect();
+        leaveEl.click();
+      }
     });
   });
 
-  let config = {
+  const observerConfig = {
     childList: true
   };
 
-  observer.observe(log, config);
-
-  if (fightEnd) {
-    observer.disconnect();
-  }
-})(4000);
+  observer.observe(combatLog, observerConfig);
+})(config);
