b6fee6181e70ad27b7e6e544ee52b70706f962ed
pure-js <3690368+pure-js@users.noreply.github.com>
2018-05-17 17:13:52 +0300

    total refactoring - meet maximum call stack trouble

--- src/walk/walker.js
+++ src/walk/walker.js
@@ -1,74 +1,117 @@
+function getRandomIntInclusive(min, max) {
+  // const minimum = Math.ceil(min);
+  // const maximum = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getCheerfulness(doc) {
+  const $cheerfulness = doc.getElementById('hru').innerHTML;
+  return Number($cheerfulness.split(' ')[1].slice(0, -1));
+}
+
+function getDirectionById(id) {
+  let direction;
+  switch (id) {
+    case 'd1':
+      direction = 'north';
+      break;
+    case 'd3':
+      direction = 'west';
+      break;
+    case 'd5':
+      direction = 'east';
+      break;
+    case 'd7':
+      direction = 'south';
+      break;
+  }
+  return direction;
+}
+
+function getIdByDirection(direction) {
+  let id;
+  switch (direction) {
+    case 'north':
+      id = 'd1';
+      break;
+    case 'west':
+      id = 'd3';
+      break;
+    case 'east':
+      id = 'd5';
+      break;
+    case 'south':
+      id = 'd7';
+      break;
+  }
+  return id;
+}
+
+function directions(doc) {
+  const availableDirections = [];
+  for (let i = 1; i < 8; i += 2) {
+    const $current = doc.getElementById(`d${i}`).childNodes[0];
+    if ($current.title.length > 0) {
+      const { id } = $current.parentElement;
+      availableDirections.push(getDirectionById(id));
+    }
+  }
+  console.log(availableDirections);
+  return availableDirections;
+}
+
+function pick(doc) {
+  const item = doc.getElementById('picks').querySelectorAll('tr td img')[0];
+  if (item) {
+    item.click();
+    setTimeout(pick, getRandomIntInclusive(170, 280));
+  }
+}
+
+function lookAround(doc) {
+  const picks = doc.getElementById('picks').childNodes.length;
+  if (picks > 0) {
+    pick(doc);
+  }
+}
+
+function randomWay(ways) {
+  const random = getRandomIntInclusive(0, ways.length - 1);
+  return ways[random];
+}
+
+function step(doc) {
+  const ways = directions(doc);
+  const id = getIdByDirection(randomWay(ways));
+  doc.getElementById(id).childNodes[0].click();
+}
+
+function findCaptcha(doc) {
+  const captcha = doc.getElementById('cod').style.display;
+  return (captcha !== 'none');
+}
+
+let waitBeforeExecuting = getRandomIntInclusive(200, 1000);
+
+function walk(doc) {
+  if (!findCaptcha(doc)) {
+    const cheers = getCheerfulness(doc);
+    if (cheers >= 5) {
+      lookAround(doc);
+      step(doc);
+      waitBeforeExecuting += getRandomIntInclusive(200, 1000);
+      console.log(waitBeforeExecuting);
+      setTimeout(walk(doc), waitBeforeExecuting);
+    } else {
+      const wait = (100 - cheers) * 1000;
+      setTimeout(walk(doc), wait);
+    }
+  }
+}
+
 function walker() {
   const $location = document.getElementById('loc').contentWindow.document;
   const $noCombat = $location.getElementsByName('no_combat')[0].contentWindow.document;
-  const config = {
-    width: 200,
-    height: 200,
-  };
-
-  function cheerfulness() {
-    const $cheerfulness = $noCombat.getElementById('hru').innerHTML;
-    const cheerfulness = Number($cheerfulness.split(' ')[1].slice(0, -1));
-    return cheerfulness;
-  }
-
-  function directions() {
-    const arr = [];
-    for (let i = 1; i < 8; i += 2) {
-      const $current = $noCombat.getElementById(`d${i}`).childNodes[0];
-      if ($current.title.length > 0) {
-        arr.push($current.parentElement.id);
-      }
-    }
-    return arr;
-  }
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  function pick() {
-    const item = $noCombat.getElementById('picks').querySelectorAll('tr td img')[0];
-    if (item) {
-      item.click();
-      setTimeout(pick, getRandomInt(200, 280));
-    }
-  }
-
-  function lookAround() {
-    const picks = $noCombat.getElementById('picks').childNodes.length;
-    if (picks > 0) {
-      pick();
-    }
-  }
-
-  function takeStep() {
-    const ways = directions();
-    const random = getRandomInt(0, ways.length - 1);
-    const randomWay = ways[random];
-    $noCombat.getElementById(randomWay).childNodes[0].click();
-  }
-
-  function findCaptcha() {
-    const captcha = $noCombat.getElementById('cod').style.display;
-    return (captcha !== 'none');
-  }
-
-  function go() {
-    if (findCaptcha()) {
-      return false;
-    }
-    const cheers = cheerfulness();
-    if (cheers >= 5) {
-      lookAround();
-      takeStep();
-      setTimeout(go, getRandomInt(600, 1000));
-    } else {
-      const wait = (100 - cheers) * 1000;
-      setTimeout(go, wait);
-    }
-  }
-
-  go();
+  walk($noCombat);
 }
 walker();
